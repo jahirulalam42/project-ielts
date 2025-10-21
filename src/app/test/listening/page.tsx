f5140b7979a99fde779413821b8c6c6cde9ceb40
A S M Morshedul Hoque <asm21612@gmail.com>
2025-10-21 11:56:02 +0600

    3 tests per row

--- src/app/test/listening/page.tsx
+++ src/app/test/listening/page.tsx
@@ -153,7 +153,7 @@ const ListeningPage: React.FC = () => {
           </div>
         ) : filteredTests.length > 0 ? (
           <div>
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
               {filteredTests.map((test) => (
                 <div
                   key={test._id}
