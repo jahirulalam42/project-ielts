b86a33978ca03276e4228a33ef48eda969206137
MD.Jahirul Alam Sarkar <jahirulalam41@gmail.com>
2026-02-03 22:44:37 +0600

    Signup/Signin design changed

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -65,7 +65,7 @@ const Navbar: React.FC = () => {
         !pathName.startsWith("/test/listening/") &&
         !pathName.startsWith("/admin") &&
         // Show navbar on sign-in page, hide for other /user routes
-        !(pathName.startsWith("/user/") && pathName !== "/user/signin") &&
+        !(pathName.startsWith("/user/") && pathName !== "/user/signin/") &&
         !pathName.startsWith("/writing-samples/") && (
           <nav className="bg-gray-200 border-b border-gray-100 shadow-sm">
             <div className="container mx-auto px-4">
